c23f4f6cdeea1e35d110f7accc75a3d447a562b4
kalaysolay <ashat_boztaev@mail.ru>
2025-06-04 23:55:22 +0500

    trying parameters

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -10,6 +10,12 @@ public class HelloWorld {
         });
         // Описываем, что загрузится по адресу /
         app.get("/", ctx -> ctx.result("Hello World"));
+        app.get("/hello", ctx -> {
+            var name = ctx.queryParam("name");
+            var prof = ctx.queryParamAsClass("prof", String.class).getOrDefault("");
+            ctx.result("Hello, " + name + " you're " + prof);
+
+        });
         app.start(7070); // Стартуем веб-сервер
     }
 }
